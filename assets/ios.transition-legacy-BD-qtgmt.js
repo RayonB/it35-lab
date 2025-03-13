26a0d0613933407c75d2847c4c1e616f0d313509
RayonB <rinevarayon@gmail.com>
2025-03-13 08:01:50 +0800

    Updates

--- assets/ios.transition-legacy-BD-qtgmt.js
+++ assets/ios.transition-legacy-BD-qtgmt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DMgXpdqB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CT3TdOm9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
