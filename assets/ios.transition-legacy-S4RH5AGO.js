874274a53ed9bcf42a8877f75cbe2d307f5f5131
RayonB <rinevarayon@gmail.com>
2025-04-13 00:49:52 +0800

    Updates

--- assets/ios.transition-legacy-S4RH5AGO.js
+++ assets/ios.transition-legacy-S4RH5AGO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgpkkWbW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BQbZWBIo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
