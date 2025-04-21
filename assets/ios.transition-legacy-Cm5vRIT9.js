ed9a0867da6d4287936ab76b2f3d352300e2c04c
RayonB <rinevarayon@gmail.com>
2025-04-21 23:45:18 +0800

    Updates

--- assets/ios.transition-legacy-Cm5vRIT9.js
+++ assets/ios.transition-legacy-Cm5vRIT9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DjH1Rdmn.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CsxLqsen.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
