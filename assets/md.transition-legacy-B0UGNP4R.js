f37941e2311529093cc9917d36d01973dff38511
RayonB <rinevarayon@gmail.com>
2025-04-21 23:31:09 +0800

    Updates

--- assets/md.transition-legacy-B0UGNP4R.js
+++ assets/md.transition-legacy-B0UGNP4R.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BuH9fnxR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DjH1Rdmn.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
