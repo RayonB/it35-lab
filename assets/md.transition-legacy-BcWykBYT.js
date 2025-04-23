977dd4e595d6b190a6f76da93d1c5672a6d1bcf7
RayonB <rinevarayon@gmail.com>
2025-04-23 14:12:09 +0800

    Updates

--- assets/md.transition-legacy-BcWykBYT.js
+++ assets/md.transition-legacy-BcWykBYT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-VcfWT2X8.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-QWh7-WrH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
